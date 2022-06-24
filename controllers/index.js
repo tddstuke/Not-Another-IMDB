3faa76a4580e65abe6b5b7481ebe8a8157a6360a
Todd Stuke <tddstuke@gmail.com>
2022-06-23 22:15:53 -0500

    corrected homeroute in controller/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,9 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
+const homeRoutes = require("./homepage-route");
+router.use("/", homeRoutes);
+
 router.use((req, res) => {
   res.status(404).end();
 });
