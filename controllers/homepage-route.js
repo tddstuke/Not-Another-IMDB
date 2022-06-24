8f3ad0c046635068e9f0e36d2be3c41f193cd354
Jacob  Bauer <jacobbauer@Jacobs-MacBook-Air.local>
2022-06-23 21:22:23 -0500

    new changes

--- controllers/homepage-route.js
+++ controllers/homepage-route.js
@@ -1,17 +1,17 @@
-const router = require("express").Router();
-const sequelize = require("../config/connection");
-const { List, Movie, User } = require("../models");
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { List, Movie, User } = require('../models');
 
-router.get("/", async (req, res) => {
-  res.render("homepage");
+router.get('/', async (req, res) => {
+	res.render('homepage');
 });
 
-router.get("/login", (req, res) => {
-  // if (req.session.loggedIn) {
-  //   res.redirect("/");
-  //   return;
-  // }
-  res.render("login");
+router.get('/login', (req, res) => {
+	// if (req.session.loggedIn) {
+	//   res.redirect("/");
+	//   return;
+	// }
+	res.render('login');
 });
 
 module.exports = router;
