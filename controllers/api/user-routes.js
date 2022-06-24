8c830bd66b44054605eaef986f3864b2b684f868
CHalseth <Corey.Halseth@gmail.com>
2022-06-23 19:15:23 -0500

    altered user model

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -18,26 +18,26 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    include: [
-      {
-        model: Post,
-        attributes: ['id', 'title', 'post_url', 'created_at']
-      },
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'created_at'],
-        include: {
-          model: Post,
-          attributes: ['title']
-        }
-      },
-      {
-        model: Post,
-        attributes: ['title'],
-        through: Vote,
-        as: 'voted_posts'
-      }
-    ]
+    // include: [
+    //   {
+    //     model: Post,
+    //     attributes: ['id', 'title', 'post_url', 'created_at']
+    //   },
+    //   {
+    //     model: Comment,
+    //     attributes: ['id', 'comment_text', 'created_at'],
+    //     include: {
+    //       model: Post,
+    //       attributes: ['title']
+    //     }
+    //   },
+    //   {
+    //     model: Post,
+    //     attributes: ['title'],
+    //     through: Vote,
+    //     as: 'voted_posts'
+    //   }
+    // ]
   })
     .then(dbUserData => {
       if (!dbUserData) {
