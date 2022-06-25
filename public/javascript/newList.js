d2d950677b4dff31aa614459b22707b9d17ba341
Jacob  Bauer <jacobbauer@Jacobs-Air.hitronhub.home>
2022-06-25 14:31:25 -0500

    create connection to between routes/js/handlebars

---
+++ public/javascript/newList.js
@@ -0,0 +1,29 @@
+async function addMovie(event, movie_info) {
+	event.preventDefault();
+	//   event.preventDefault();
+	//   const list_id = 1;
+	const title = movie_info.title;
+	const movie_id = movie_info.id;
+
+	console.log(movie_id);
+	const response = await fetch('controller/api/list', {
+		method: 'POST',
+		body: JSON.stringify({
+			//   list_id,
+			movie_id,
+			title,
+		}),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	if (response.ok) {
+		document.location.replace('/dashboard');
+	} else {
+		alert(response.statusText);
+	}
+}
+
+document
+	.querySelector('#watch-button')
+	.addEventListener('click', searchButtonHandler);
