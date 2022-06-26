2c898564a27e07f00717535d3dd6ad53051f379c
KevinJ3259 <kmjordan2009.kj@gmail.com>
2022-06-26 12:29:04 -0500

    fetchby id code

--- services/movie-service.js
+++ services/movie-service.js
@@ -10,14 +10,9 @@ module.exports = {
     const query_name = movieName.split(" ").join("+");
     return axios.get(`${apiUrl}${key}&query=${query_name}`);
   },
-<<<<<<< HEAD
-  fetchById(movieId) {
-    return axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${key}`);
-=======
 
   FetchByID(movieId) {
     const id = movieId;
     return axios.get(`${byIdUrl}${id}?api_key=${key}`);
->>>>>>> ca280d0fe9d5f57e56bf9ffb6f16261f1b481d1c
   },
 };
