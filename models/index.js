5c6b1d7264c584dd44fe2cbed89104a83350d845
Todd Stuke <tddstuke@gmail.com>
2022-06-26 13:59:20 -0500

    merge error fixes

--- models/index.js
+++ models/index.js
@@ -2,19 +2,19 @@ const User = require("./User");
 const List = require("./List");
 const Movie = require("./Movie");
 
-User.hasMany(List, {
-  foreignKey: "user_id",
-});
+// User.hasMany(List, {
+//   foreignKey: "user_id",
+// });
 
-List.belongsTo(User, {
-  foreignKey: "user_id",
-}),
-  List.hasMany(Movie, {
-    foreignKey: "list_id",
-  });
+// List.belongsTo(User, {
+//   foreignKey: "user_id",
+// }),
+//   List.hasMany(Movie, {
+//     foreignKey: "list_id",
+//   });
 
-Movie.belongsTo(List, {
-  foreignKey: "list_id",
-});
+// Movie.belongsTo(List, {
+//   foreignKey: "list_id",
+// });
 
 module.exports = { Movie, User, List };
