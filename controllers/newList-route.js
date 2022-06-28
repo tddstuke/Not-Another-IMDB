f67f39c25453c92302bb073d7722f7768fc20c35
Jacob  Bauer <jacobbauer@Jacobs-Air.hitronhub.home>
2022-06-28 17:24:14 -0500

    javascript commit out

--- controllers/newList-route.js
+++ controllers/newList-route.js
@@ -1,30 +1,30 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { List, Movie, User } = require('../models');
-const movieDataBase = require('../services/movie-service');
+// const router = require('express').Router();
+// const sequelize = require('../config/connection');
+// const { List, Movie, User } = require('../models');
+// const movieDataBase = require('../services/movie-service');
 
-router.get('/', async (req, res) => {
-	try {
-		const movies = await Movie.findAll({ raw: true });
-		console.log(movies);
-		const filledMovies = await Promise.all(
-			movies.map(async (movie) => {
-				const { data } = await movieDataBase.FetchByID(movie.movie_id);
-				return data;
-			})
-		);
-		console.log(filledMovies);
-		res.render('create-list', {
-			movies: filledMovies,
-			loggedIn: req.session.loggedIn,
-		});
-	} catch (err) {
-		console.log(err);
-	}
-});
+// router.get('/', async (req, res) => {
+// 	try {
+// 		const movies = await Movie.findAll({ raw: true });
+// 		console.log(movies);
+// 		const filledMovies = await Promise.all(
+// 			movies.map(async (movie) => {
+// 				const { data } = await movieDataBase.FetchByID(movie.movie_id);
+// 				return data;
+// 			})
+// 		);
+// 		console.log(filledMovies);
+// 		res.render('create-list', {
+// 			movies: filledMovies,
+// 			loggedIn: req.session.loggedIn,
+// 		});
+// 	} catch (err) {
+// 		console.log(err);
+// 	}
+// });
 
-router.get('/by-name:name', async (req, res) => {
-	res.render;
-});
+// router.get('/by-name:name', async (req, res) => {
+// 	res.render;
+// });
 
-module.exports = router;
+// module.exports = router;
--- public/javascript/newList.js
+++ public/javascript/newList.js
@@ -1,27 +1,27 @@
-async function addMovie(event, movie_info) {
-	event.preventDefault();
-	//   event.preventDefault();
-	//   const list_id = 1;
-	const title = movie_info.title;
-	const movie_id = movie_info.id;
+// async function addMovie(event, movie_info) {
+// 	event.preventDefault();
+// 	//   event.preventDefault();
+// 	//   const list_id = 1;
+// 	const title = movie_info.title;
+// 	const movie_id = movie_info.id;
 
-	console.log(movie_id);
-	const response = await fetch('controller/api/list', {
-		method: 'POST',
-		body: JSON.stringify({
-			//   list_id,
-			movie_id,
-			title,
-		}),
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	});
-	if (response.ok) {
-		document.location.replace('/dashboard');
-	} else {
-		alert(response.statusText);
-	}
-}
+// 	console.log(movie_id);
+// 	const response = await fetch('controller/api/list', {
+// 		method: 'POST',
+// 		body: JSON.stringify({
+// 			//   list_id,
+// 			movie_id,
+// 			title,
+// 		}),
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 		},
+// 	});
+// 	if (response.ok) {
+// 		document.location.replace('/dashboard');
+// 	} else {
+// 		alert(response.statusText);
+// 	}
+// }
 
-document.querySelector('#watch-button').addEventListener('click', addMovie);
+// document.querySelector('#watch-button').addEventListener('click', addMovie);
