0e950bc25f8e90c753639d4d659d6ae3d2fefb1b
Jacob  Bauer <jacobbauer@Jacobs-MacBook-Air.local>
2022-06-28 20:57:39 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Not Another IMDB
+
+Not Another IMDB is the movie lovers must have tool to organize your watchlists!
+
+## Description
+
+A search engine to look up movies with a detailed synapsis.
+
+## Table of Contents
+
+- [Screenshots](#screenshots)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Screenshots
+
+## Installation
+
+To be announced
+
+## Usage
+
+Link to Heroku to be announced
+
+## Credits
+
+Backend team members:
+
+- [Todd Stuke](https://github.com/tddstuke)
+
+- [Corey Halseth](https://github.com/CHalseth)
+
+Frontend team members:
+
+- [Kevin Jordan](https://github.com/KevinJ3259)
+
+- [Jake Bauer](https://github.com/Rolo2121)
+
+## License
+
+![NPM License](https://img.shields.io/npm/l/:MIT)
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+## Project Status and future versions
+
+Future versions will have the ability to create a watch list and will be able to
+categorize movies based on your personal preferences.
--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-const express = require("express");
-const routes = require("./controllers");
-const helpers = require("./utils/helper.js");
-require("dotenv").config();
+const express = require('express');
+const routes = require('./controllers');
+const helpers = require('./utils/helper.js');
+require('dotenv').config();
 
-const sequelize = require("./config/connection");
+const sequelize = require('./config/connection');
 
-const path = require("path");
-const exphbs = require("express-handlebars");
+const path = require('path');
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
-const session = require("express-session");
+const session = require('express-session');
 
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.SESSION_SECRET,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
+	secret: process.env.SESSION_SECRET,
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+	}),
 };
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
+	app.listen(PORT, () => console.log(`Now Listening on ${PORT}`));
 });
